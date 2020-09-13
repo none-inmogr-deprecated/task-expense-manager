3c3411ddb0d33d95e54578bc5d5b63de953fd9bb
Morhaf <inmogr@msn.com>
2020-09-13 23:38:39 +0800

    handle view expense

--- src/pages/ExpenseList.tsx
+++ src/pages/ExpenseList.tsx
@@ -1,7 +1,7 @@
 import { useNavigation } from '@react-navigation/native';
 import moment from 'moment';
 import React, { memo } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 import { FlexSpacer, Row, Spacer } from '../atoms';
 import { Header } from '../containers';
@@ -27,6 +27,11 @@ const styles = StyleSheet.create({
 export const ExpenseList = memo(() => {
   const navigation = useNavigation();
   const expense = useExpense();
+
+  const onPressItem = () => {
+    navigation.navigate(global.ROUTES.EXPENSE_VIEW);
+  };
+
   return (
     <View style={styles.root}>
       <Header
@@ -40,26 +45,28 @@ export const ExpenseList = memo(() => {
       />
       {expense.data.map((item, index) => {
         return (
-          <View key={index} style={styles.viewItem}>
-            <Row>
-              <IcoMoon name="coin-dollar" size={24} />
-              <Spacer space={16} isHorizontal={true} />
-              <View style={styles.textView}>
+          <Pressable key={index} onPress={onPressItem}>
+            <View style={styles.viewItem}>
+              <Row>
+                <IcoMoon name="coin-dollar" size={24} />
+                <Spacer space={16} isHorizontal={true} />
+                <View style={styles.textView}>
+                  <Text style={styles.text}>
+                    {moment(item.date).format('MM/DD')}
+                  </Text>
+                </View>
+                <FlexSpacer />
                 <Text style={styles.text}>
-                  {moment(item.date).format('MM/DD')}
+                  {new Intl.NumberFormat('en-US', {
+                    minimumFractionDigits: 2,
+                    maximumFractionDigits: 2,
+                    currency: 'USD',
+                    style: 'currency',
+                  }).format(item.amount)}
                 </Text>
-              </View>
-              <FlexSpacer />
-              <Text style={styles.text}>
-                {new Intl.NumberFormat('en-US', {
-                  minimumFractionDigits: 2,
-                  maximumFractionDigits: 2,
-                  currency: 'USD',
-                  style: 'currency',
-                }).format(item.amount)}
-              </Text>
-            </Row>
-          </View>
+              </Row>
+            </View>
+          </Pressable>
         );
       })}
     </View>
