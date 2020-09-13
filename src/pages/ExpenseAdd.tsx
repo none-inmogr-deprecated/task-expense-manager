8b066d114e774c3e1b028b7f34d32793c352752c
Morhaf <inmogr@msn.com>
2020-09-13 23:37:55 +0800

    completed add page

--- src/pages/ExpenseAdd.tsx
+++ src/pages/ExpenseAdd.tsx
@@ -1,18 +1,71 @@
-import React, { memo } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import moment from 'moment';
+import React, { memo, useState } from 'react';
+import { Alert, StyleSheet, View } from 'react-native';
+import { generateShortId } from 'short-id-generator';
+
+import { Button, FlexSpacer, Input, Spacer } from '../atoms';
+import { Header } from '../containers';
+import { useExpense } from '../contexts';
 
 const styles = StyleSheet.create({
-  safeAreaView: {
+  root: {
     height: '100%',
     width: '100%',
   },
 });
 
 export const ExpenseAdd = memo(() => {
+  const navigation = useNavigation();
+  const expense = useExpense();
+  const [value, setValue] = useState<number | undefined>(undefined);
+
+  const onChangeText = (text?: string) => {
+    if (text === undefined) {
+      setValue(undefined);
+      return;
+    }
+    const amount = parseInt(text, 10);
+    if (amount === NaN) {
+      Alert.alert('Please enter a number');
+      return;
+    }
+    setValue(amount);
+  };
+
+  const save = () => {
+    if (typeof value !== 'number') {
+      return;
+    }
+    expense.append({
+      amount: value,
+      date: moment().toISOString(),
+      id: generateShortId(),
+    });
+    navigation.navigate(global.ROUTES.EXPENSE_LIST);
+  };
+
   return (
-    <View style={styles.safeAreaView}>
-      {/* JUST PLACEHOLDER, SOME CONTENT WILL BE ADDED HERE */}
-      <Text>ExpenseAdd Page</Text>
+    <View style={styles.root}>
+      <Header
+        left={{
+          name: 'arrow-left',
+          onPress: () => {
+            if (navigation.canGoBack()) {
+              navigation.goBack();
+            }
+          },
+        }}
+        title="Create New Expense"
+      />
+      <Spacer space={32} />
+      <Input
+        value={`${value || ''}`}
+        onChangeText={onChangeText}
+        placeholder="Enter Amount"
+      />
+      <FlexSpacer />
+      <Button label="Save" onPress={save} />
     </View>
   );
 });
