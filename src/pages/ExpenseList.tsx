b70a5a5474b262c53144e905c62b0afe65907b63
Morhaf <inmogr@msn.com>
2020-09-13 23:38:24 +0800

    complated basic list

--- src/pages/ExpenseList.tsx
+++ src/pages/ExpenseList.tsx
@@ -1,18 +1,67 @@
+import { useNavigation } from '@react-navigation/native';
+import moment from 'moment';
 import React, { memo } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { FlexSpacer, Row, Spacer } from '../atoms';
+import { Header } from '../containers';
+import { useExpense } from '../contexts';
+import { IcoMoon } from '../integrations/vector-icons';
+
 const styles = StyleSheet.create({
-  safeAreaView: {
+  root: {
     height: '100%',
     width: '100%',
+  },
+  viewItem: {
+    padding: 16,
+  },
+  textView: {
+    flex: 1,
+  },
+  text: {
+    fontSize: 20,
   },
 });
 
 export const ExpenseList = memo(() => {
+  const navigation = useNavigation();
+  const expense = useExpense();
   return (
-    <View style={styles.safeAreaView}>
-      {/* JUST PLACEHOLDER, SOME CONTENT WILL BE ADDED HERE */}
-      <Text>ExpenseAdd Page</Text>
+    <View style={styles.root}>
+      <Header
+        title="Expenses"
+        right={{
+          name: 'folder-plus',
+          onPress: () => {
+            navigation.navigate(global.ROUTES.EXPENSE_ADD);
+          },
+        }}
+      />
+      {expense.data.map((item, index) => {
+        return (
+          <View key={index} style={styles.viewItem}>
+            <Row>
+              <IcoMoon name="coin-dollar" size={24} />
+              <Spacer space={16} isHorizontal={true} />
+              <View style={styles.textView}>
+                <Text style={styles.text}>
+                  {moment(item.date).format('MM/DD')}
+                </Text>
+              </View>
+              <FlexSpacer />
+              <Text style={styles.text}>
+                {new Intl.NumberFormat('en-US', {
+                  minimumFractionDigits: 2,
+                  maximumFractionDigits: 2,
+                  currency: 'USD',
+                  style: 'currency',
+                }).format(item.amount)}
+              </Text>
+            </Row>
+          </View>
+        );
+      })}
     </View>
   );
 });
