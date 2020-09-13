ec06711997d3e0c3266d62431c33f50f3f3f9cf1
Morhaf <inmogr@msn.com>
2020-09-13 23:22:44 +0800

    basic native setup

--- android/app/src/main/java/com/taskexpensemanager/MainActivity.java
+++ android/app/src/main/java/com/taskexpensemanager/MainActivity.java
@@ -1,9 +1,14 @@
 package com.taskexpensemanager;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
+// react-native-splash-screen >= 0.3.1
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
-
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
@@ -11,5 +16,15 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "TaskExpenseManager";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+      return new ReactActivityDelegate(this, getMainComponentName()) {
+        @Override
+          protected ReactRootView createRootView() {
+            return new RNGestureHandlerEnabledRootView(MainActivity.this);
+          }
+      };
   }
 }
