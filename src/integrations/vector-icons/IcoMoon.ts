cf9f00c756cd1ff7b38b581e6c429cf8ea6d7559
Morhaf <inmogr@msn.com>
2020-09-13 23:24:53 +0800

    setup icomoon icons

---
+++ src/integrations/vector-icons/IcoMoon.ts
@@ -0,0 +1,5 @@
+import { createIconSetFromIcoMoon } from 'react-native-vector-icons';
+
+import { IcoMoonSelection } from './IcoMoonSelection';
+
+export const IcoMoon = createIconSetFromIcoMoon(IcoMoonSelection, 'icomoon', 'icomoon.ttf');
